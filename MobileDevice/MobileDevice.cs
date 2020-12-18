bf5b75ddf40befd2553766f6b0bfc8869f0a2725
vaske <shomi199870@gmail.com>
2020-12-18 19:05:38 +0100

    Fixed iTunes DLL path
    Fixed iTunes DLL path

--- MobileDevice/MobileDevice.cs
+++ MobileDevice/MobileDevice.cs
@@ -9,7 +9,7 @@ namespace MK.MobileDevice
 
     internal class MobileDevice
     {
-        private static readonly DirectoryInfo ApplicationSupportDirectory = new DirectoryInfo(Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Apple Inc.\Apple Application Support", "InstallDir", Environment.CurrentDirectory).ToString());
+        private static readonly DirectoryInfo ApplicationSupportDirectory = new DirectoryInfo(Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Apple Inc.\Apple Mobile Device Support", "InstallDir", Environment.CurrentDirectory).ToString());
         private const string iTMDDLLPath = "iTunesMobileDevice.dll";
         private static readonly FileInfo iTunesMobileDeviceFile = new FileInfo(Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Apple Inc.\Apple Mobile Device Support\Shared", "iTunesMobileDeviceDLL", iTMDDLLPath).ToString());
 
